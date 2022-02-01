8713cdb98c57c97da39fb1eb91257734d6c6c4b6
Pankhuri <pankhuri@Pankhuris-MacBook-Air.local>
2022-02-01 11:15:17 +0530

    commit

---
+++ crud operations/CRUD/src/main/java/com/example/CRUD/dao/TicketDao.java
@@ -0,0 +1,9 @@
+package com.example.CRUD.dao;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.CRUD.model.Ticket;
+@Repository
+public interface TicketDao extends CrudRepository<Ticket,Integer> {
+
+}
